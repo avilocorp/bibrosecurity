c2652235565bc668155d37c1364a592360799157
JorgeNaranjo90 <jorge020890@gmail.com>
2015-07-11 00:35:45 -0500

    Add userType table in database

---
+++ bibrosecurity/src/java/com/bibrosecurity/models/User.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bibrosecurity.models;
+
+/**
+ *
+ * @author taxque
+ */
+public class User {
+
+    private int id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private int partner_id;
+    private boolean activo;
+    
+
+
+    
+}
