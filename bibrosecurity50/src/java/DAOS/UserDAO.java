90665e69b9d45a231eba9d757a609a04e16bde26
JorgeNaranjo90 <jorge020890@gmail.com>
2015-08-13 09:33:54 -0500

    IMP: Add other files

---
+++ bibrosecurity50/src/java/DAOS/UserDAO.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAOS;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import models.User;
+
+/**
+ *
+ * @author taxque
+ */
+public class UserDAO extends DAO{
+    
+    public User login(String email, String password) throws Exception {
+        ResultSet rs;
+        User user = new User();
+        try {
+            this.connect();
+            PreparedStatement st = this.getCon().prepareStatement(
+                    "select * from user where email=? and password=? and active=1;"            );
+            st.setString(1, email);
+            st.setString(2, password);
+            rs = st.executeQuery();
+            while (rs.next()) {
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setLastName(rs.getString("lastName"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+                user.setUserTypeid(rs.getInt("userType_id"));
+                user.setActive(rs.getBoolean("active"));
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.close();
+        }
+        return user;
+    }
+    
+    public void registrar(User user) throws Exception {
+        try {
+            this.connect();
+            PreparedStatement st = this.getCon().prepareStatement("insert into user (name, lastName, email, password, userType_id) values(?,?,?,?,?)");
+            st.setString(1, user.getName()); 
+            st.setString(2, user.getLastName());
+            st.setString(3, user.getEmail());
+            st.setString(4, user.getPassword());
+            st.setInt(5, user.getUserTypeid());
+            st.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            this.close();
+        }
+    }
+            
+}
---
+++ bibrosecurity50/src/java/DAOS/shoppingSaleOrderDAO.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAOS;
+
+import java.sql.PreparedStatement;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import models.Item;
+import models.SaleOrder;
+import models.SaleOrderLine;
+import java.util.Date;
+
+/**
+ *
+ * @author taxque
+ */
+@ManagedBean
+@RequestScoped
+public class shoppingSaleOrderDAO extends DAO{
+
+    /**
+     * Creates a new instance of shoppingSaleOrderDAO
+     */
+    
+    Date fecha = new Date();
+    public shoppingSaleOrderDAO() {
+    }
+    
+    public int registrarSaleOrder (SaleOrder so) throws Exception{
+        int sale_id = 0;
+        try {
+            this.connect();
+            String query="insert into saleOrder ( subTotal, discount, total, type, partner_id)"
+                    + "values (?,?,?,?,?); ";
+            PreparedStatement st=this.getCon().prepareStatement(query);
+            st.setDouble(1, so.getSubTotal());
+            st.setDouble(2, so.getDiscount());
+            st.setDouble(3, so.getTotal());
+            st.setString(4, so.getType());
+            st.setInt(5, so.getPartnerId());
+
+            sale_id = st.executeUpdate();
+            System.out.println("****************************************************");
+        } catch (Exception e) {
+             throw e;
+        }finally{
+            this.close();
+        } 
+        return sale_id;
+    }
+        
+    public void registrarSaleOrderLine (Item item, int sale_order) throws Exception{
+        try {
+            this.connect();
+            String query="insert into saleOrderLine (saleOrder_id, qty, unit_price, subtotal, product_id)"
+                    + "values (?,?,?,?,?); ";
+            PreparedStatement st=this.getCon().prepareStatement(query);
+            st.setInt(1, sale_order);
+            st.setInt(2, item.getQuantity());
+            st.setDouble(3, item.getP().getPurchasePrice());
+            st.setDouble(4, item.getSubtotal());
+            st.setInt(5, item.getP().getId());
+
+            st.executeUpdate();
+        } catch (Exception e) {
+             throw e;
+        }finally{
+            this.close();
+        } 
+    }
+    
+}
